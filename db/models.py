ddd75ff52831f866281b2d97853175b9c5d87633
titoiriepal <titoiriepal@hotmail.com>
2026-02-05 13:49:24 +0100

    capacity to property for CinemaHall

--- db/models.py
+++ db/models.py
@@ -34,6 +34,7 @@ class CinemaHall(models.Model):
     def __str__(self) -> str:
         return self.name
 
+    @property
     def capacity(self) -> int:
         return self.rows * self.seats_in_row
 
