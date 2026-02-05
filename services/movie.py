e26789ad8f04a73ca839e2b7ac46982a64d9740f
titoiriepal <titoiriepal@hotmail.com>
2026-02-05 14:56:26 +0100

    First changes for flake8: All lines under 80 characters

--- services/movie.py
+++ services/movie.py
@@ -9,11 +9,18 @@ def get_movies(
     if genres_ids is None and actors_ids is None:
         return Movie.objects.all()
     if genres_ids and actors_ids:
-        return Movie.objects.filter(genres__id__in=genres_ids, actors__id__in=actors_ids).distinct()
+        return Movie.objects.filter(
+            genres__id__in=genres_ids,
+            actors__id__in=actors_ids,
+        ).distinct()
     if genres_ids:
-        return Movie.objects.filter(genres__id__in=genres_ids).distinct()
+        return Movie.objects.filter(
+            genres__id__in=genres_ids,
+        ).distinct()
     if actors_ids:
-        return Movie.objects.filter(actors__id__in=actors_ids).distinct()
+        return Movie.objects.filter(
+            actors__id__in=actors_ids,
+        ).distinct()
     return None
 
 def get_movie_by_id(movie_id: int) -> Movie:
